4231379b7c7b3a2eaa2957f56b10460d17f9915d
evanmmitchell <evan.m.mitchell@gmail.com>
2019-03-04 12:52:46 -0800

    Update Receiver.ino

--- Receiver.ino
+++ Receiver.ino
@@ -1,33 +1,60 @@
 int inputPin = 2;
+int timeUnit = 200;
+int testFrequency = 10;
+boolean initiated = false;
+unsigned long previousMillis;
 
-const int dot = 1;
-const int dash = 2;
+const int DOT = 1;
+const int DASH = 2;
 
-const int CHARACTERS[][10] = {
+const int MORSE_CODE[][10] = {
   {DOT, DASH},
   {DASH, DOT, DOT, DOT},
-  
+  {DASH, DOT, DASH, DOT},
+  {DASH, DOT, DOT},
+  {DOT},
+  {DOT, DOT, DASH, DOT},
+  {DASH, DASH, DOT},
+  {DOT, DOT, DOT, DOT},
+  {DOT, DOT},
+  {DOT, DASH, DASH, DASH},
+  {DASH, DOT, DASH},
+  {DOT, DASH, DOT, DOT},
+  {DASH, DASH},
+  {DASH, DOT},
+  {DASH, DASH, DASH},
+  {DOT,DASH, DASH, DOT},
+  {DASH, DASH, DOT, DASH},
+  {DOT, DASH, DOT},
+  {DOT, DOT, DOT},
+  {DASH},
+  {DOT, DOT, DASH},
+  {DOT, DOT, DOT, DASH},
+  {DOT, DASH, DASH},
+  {DASH, DOT, DOT, DASH},
+  {DASH, DOT, DASH, DASH},
+  {DASH, DASH, DOT, DOT},
+  {DOT, DASH, DASH, DASH, DASH},
+  {DOT, DOT, DASH, DASH, DASH},
+  {DOT, DOT, DOT, DASH, DASH},
+  {DOT, DOT, DOT, DOT, DASH},
+  {DOT, DOT, DOT, DOT, DOT},
+  {DASH, DOT, DOT, DOT, DOT},
+  {DASH, DASH, DOT, DOT, DOT},
+  {DASH, DASH, DASH, DOT, DOT},
+  {DASH, DASH, DASH, DASH, DOT},
+  {DASH, DASH, DASH, DASH, DASH},
+  {DOT, DASH, DOT, DASH, DOT, DASH},
+  {DASH, DASH, DOT, DOT, DASH, DASH},
+  {DOT, DOT, DASH, DASH, DOT, DOT},
+  {DASH, DOT, DASH, DOT, DASH, DASH},
+  {DASH, DOT, DASH, DASH, DOT},
+  {DASH, DOT, DASH, DASH, DOT, DASH},
+  {DASH, DASH, DASH, DOT, DOT},
+  {DASH, DOT, DASH, DOT, DASH}
 };
 
-const int A = 0, B = 1, C = 3, D = 4, E = 5, F = 6, G = 7, H = 8, I = 9, J = 10, K = 11, L = 12, M = 13, N = 14, O = 15, P = 16, Q = 17, R = 18, S = 19, T = 20, U = 21, V = 22, W = 23, X = 24, Z = 25, ONE = 26, TWO = 27, THREE = 28, FOUR = 29, FIVE = 30, SIX = 31, SEVEN = 32, EIGHT = 33, NINE = 34, ZERO = 35, PERIOD = 36, COMMA = 37, QUESTION = 38, EXCLAMATION = 39, OPEN_PARENTHESIS = 40, CLOSE_PARENTHESIS = 41, COLON = 42, SEMICOLON = 43;
-
-void dot() {
-  digitalWrite(ledPin, HIGH);
-  delay(timeUnit);
-  digitalWrite(ledPin, LOW);
-}
-
-void dash() {
-  digitalWrite(ledPin, HIGH);
-  delay(timeUnit * 3);
-  digitalWrite(ledPin, LOW);
-}
-
-void receiveMorseCode() {
-  for (int i = 0; i < sizeof(characters); i++) {
-    
-  }
-}
+const char CHARACTERS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z', '1', '2','3', '4', '5', '6', '7', '8', '9', '0', '.', ',', '?', '!','(', ')', ':', ';'};
 
 void setup() {
   Serial.begin(9600);
@@ -35,7 +62,59 @@ void setup() {
 }
 
 void loop() {
-  receiveMorseCode();
-  delay(timeUnit * 28);
-  Serial.println();
+  int offCounter = 0;
+  while (digitalRead(inputPin) == LOW) {
+    delay(timeUnit / testFrequency);
+    offCounter++;
+  }
+  if (offCounter > testFrequency * 3 && offCounter <= testFrequency * 8 && initiated) {
+    Serial.print(' ');
+  } else if (offCounter > testFrequency * 8 && initiated) {
+    Serial.println();
+  }
+
+  initiated = true;
+  previousMillis = millis();
+  int currentMorseCode[10];
+  int index = 0;
+  int numberOfDots = 0;
+  int numberOfDashes = 0;
+
+  while ((millis() - timeUnit - previousMillis) < (timeUnit * numberOfDots * 2 + timeUnit * numberOfDashes * 4)) {
+    if (index < sizeof(currentMorseCode)) {
+      int onCounter = 0;
+      while (digitalRead(inputPin) == HIGH){
+        delay(timeUnit / testFrequency);
+        onCounter++;
+      }
+      if (onCounter <= testFrequency) {
+        currentMorseCode[index] = DOT;
+        numberOfDots++;
+        index++;
+      } else if (onCounter <= testFrequency * 3) {
+        currentMorseCode[index] = DASH;
+        numberOfDashes++;
+        index++;
+      } else {
+        index = sizeof(currentMorseCode);
+      }
+      offCounter = 0;
+      while (digitalRead(inputPin) == LOW && offCounter <= testFrequency) {
+        delay(timeUnit / testFrequency);
+        offCounter++;
+      }
+    } else {
+      Serial.println("ERROR: Check value of timeUnit");
+    }
+  }
+
+  for (int i = 0; i < sizeof(MORSE_CODE); i++) {
+    for (int j = 0; j < sizeof(currentMorseCode) && MORSE_CODE[i][j] == currentMorseCode[j]; j++) {
+      if (currentMorseCode[j] == 0) {
+        Serial.print(CHARACTERS[i]);
+        i = sizeof(MORSE_CODE);
+        break;
+      }
+    }
+  }
 }
