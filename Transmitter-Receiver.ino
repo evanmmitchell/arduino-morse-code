71525ca74d80ecb85f746957652d2a77de4dfba3
evanmmitchell <evan.m.mitchell@gmail.com>
2019-03-04 13:36:35 -0800

    Update Transmitter-Receiver.ino

--- Transmitter-Receiver.ino
+++ Transmitter-Receiver.ino
@@ -1,15 +1,12 @@
-/* 
+/*
  * This Arduino sketch allows two boards to communicate with each other using Morse Code.
- *
+ * 
  * After uploading this sketch to two Arduino boards, a user may send a string input to one of the boards via the 
  * Serial Monitor. This board then transmits in Morse Code each character of the input using an infrared emitter. 
  * The other board simultaneously receives this message with an infrared sensor, converting the received Morse Code 
  * back into English and printing it to the Serial Monitor. When the transmission is complete, new string inputs 
- * may continue to be sent to either board to be transmitted and received as described above. 
+ * may continue to be sent to either board to be transmitted and received as described above.
  * Note: If inputs are sent to both boards at the same time, neither will be received.
- *
- * String input from Serial Monitor (input) --> Morse Code transmitted by IR emitter (output) 
- * Morse Code received by IR sensor (input) --> English characters printed to the Serial Monitor (output) 
  */
 
 
@@ -187,5 +184,3 @@ void loop() {
     digitalWrite(TRANSMITTING_LED_PIN, LOW);
   }
 }
-
-
